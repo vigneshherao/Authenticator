25ab1550bb51cd96345ef25464e943b18ebe614f
vigneshherao <vigneshfornavy@gmail.com>
2025-03-19 20:29:03 +0530

    implemented login service added cookies

--- Authenticator/controller/user.controller.js
+++ Authenticator/controller/user.controller.js
@@ -1,6 +1,12 @@
 import User from "../models/User.model.js";
 import crypto from "crypto";
 import nodemailer from "nodemailer";
+import bcyrpt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import exp from "constants";
+dotenv.config();
 
 const register = async (req, res) => {
   const { username, email, password, role } = req.body;
@@ -16,10 +22,13 @@ const register = async (req, res) => {
       res.status(400).json({ message: "User already exists" });
     }
 
+    console.log(password);
+    const hashedPassword = await bcyrpt.hash(password, 10);
+    console.log(hashedPassword);
     const user = new User({
       username,
       email,
-      password,
+      password: hashedPassword,
       role,
     });
 
@@ -88,4 +97,49 @@ const verifyUser = async (req, res) => {
   } catch (error) {}
 };
 
-export { register, verifyUser };
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "Please fill in all fields" });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isPasswordMatch = await bcyrpt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      return res
+        .status(400)
+        .json({ message: "Password is not correct credentials" });
+    }
+
+    if (!user.isVerified) {
+      return res.status(400).json({ message: "Please verify your account!" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
+
+    const cookieOptions = {
+      httpOnly: true,
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    };
+
+    res.cookie("token", token, cookieOptions);
+
+    res.status(200).json({ message: "User logged in successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Server Error", error });
+  }
+};
+
+export { register, verifyUser, login };
--- Authenticator/routes/user.routes.js
+++ Authenticator/routes/user.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
 const Router = express.Router();
-import { register, verifyUser } from "../controller/user.controller.js";
+import { register, verifyUser, login } from "../controller/user.controller.js";
 
 Router.post("/register", register);
 Router.get("/verify/:token", verifyUser);
+Router.post("/login", login);
 
 export default Router;
